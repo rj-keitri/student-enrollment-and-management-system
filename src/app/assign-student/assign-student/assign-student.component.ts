89c30562c00e533b7a97c8f96ad725c4552b08e1
rj-keitri <roberto.valdez@keitri.com>
2025-03-17 08:40:31 +0800

    Created login, student-registration, assign-student, class-management, subfoldedrs and app.module.ts inside src/app directory.

---
+++ src/app/assign-student/assign-student/assign-student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assign-student',
+  imports: [],
+  templateUrl: './assign-student.component.html',
+  styleUrl: './assign-student.component.css'
+})
+export class AssignStudentComponent {
+
+}
---
+++ src/app/class-management/class-management/class-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-management',
+  imports: [],
+  templateUrl: './class-management.component.html',
+  styleUrl: './class-management.component.css'
+})
+export class ClassManagementComponent {
+
+}
