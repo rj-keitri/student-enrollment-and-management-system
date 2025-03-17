45779f832bb029325193a9c51c2748c1699b0e4c
rj-keitri <roberto.valdez@keitri.com>
2025-03-17 09:26:17 +0800

    Revisedlogin.component.ts' contents.

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    // Implement your login logic here
+  }
 }
