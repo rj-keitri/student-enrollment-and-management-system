9584a2995f38d3d5d2fde1bd6dd16b05159a4d0f
rj-keitri <roberto.valdez@keitri.com>
2025-03-17 10:59:03 +0800

    test codes in app.routing.modules.ts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
 import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 import { AssignStudentComponent } from './assign-student/assign-student.component';
 import { ClassManagementComponent } from './class-management/class-management.component';
 
 const routes: Routes = [
-  { path: 'login' , component: LoginComponent },
-  { path: 'register' , component: StudentRegistrationComponent },
-  { path: 'assign-student', component: AssignStudentComponent },
-  { path: 'class-management', component: ClassManagementComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // { path: 'login' , component: LoginComponent },
+  // { path: 'register' , component: StudentRegistrationComponent },
+  // { path: 'assign-student', component: AssignStudentComponent },
+  // { path: 'class-management', component: ClassManagementComponent },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    { path: '', component: HomeComponent }
 ];
+
 
 @NgModule({
 imports: [RouterModule.forRoot(routes)],
