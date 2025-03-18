3b74a69bc56e868e792dff19d5e7d2eb26862a21
rj-keitri <roberto.valdez@keitri.com>
2025-03-18 09:00:40 +0800

    Test modification on app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // Import your routing module
 import { AppComponent } from './app.component'; // Import your main app component
+import { HomeComponent } from './home/home.component'; // Adjust the path as necessary
 
 @NgModule({
-  
+
   imports: [
     BrowserModule,
     AppRoutingModule // Include the routing module
