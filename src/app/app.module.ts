21ef423a55c4ab9b795498a5c126bd5d2d086a82
rj-keitri <roberto.valdez@keitri.com>
2025-03-18 08:51:01 +0800

    Test modifications on app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { AppRoutingModule } from './app-routing.module'; // Import your routing 
 import { AppComponent } from './app.component'; // Import your main app component
 
 @NgModule({
+
+  declarations: [
+    AppComponent,
+    // HomeComponent can be declared here if not using standalone
+  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule // Include the routing module
