860c57b153a1f30d05bd3a78c192064a67f0ac79
rj-keitri <roberto.valdez@keitri.com>
2025-03-18 08:52:35 +0800

    Revert to last state of app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { AppRoutingModule } from './app-routing.module'; // Import your routing 
 import { AppComponent } from './app.component'; // Import your main app component
 
 @NgModule({
-
-  declarations: [
-    AppComponent,
-    // HomeComponent can be declared here if not using standalone
-  ],
-
+  
   imports: [
     BrowserModule,
     AppRoutingModule // Include the routing module
